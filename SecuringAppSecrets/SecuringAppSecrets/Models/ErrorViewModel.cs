2e42c6bff1b36a7a8430e9ee4b4d40e1ee4947de
rupeshtech <rupeshwillwin@gmail.com>
2018-06-06 15:15:23 +0200

    intial commit

---
+++ SecuringAppSecrets/SecuringAppSecrets/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SecuringAppSecrets.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
